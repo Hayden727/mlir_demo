f42a98a9f9a9cebdfb55ce5615c16bdd758c17c8
hayden-brown <hongcc727@163.com>
2024-04-11 15:15:32 +0800

    rebase

--- build/CMakeFiles/mlir_demo.dir/DependInfo.cmake
+++ build/CMakeFiles/mlir_demo.dir/DependInfo.cmake
@@ -11,8 +11,12 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/hayden/prj/mlir_demo/lib/LasmC/LasmCDialect.cpp" "CMakeFiles/mlir_demo.dir/lib/LasmC/LasmCDialect.cpp.o" "gcc" "CMakeFiles/mlir_demo.dir/lib/LasmC/LasmCDialect.cpp.o.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
